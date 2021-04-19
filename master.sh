97f3e45065fdc5e1b6da9cf352e074d5e59033d6
tstark7952 <38063000+tstark7952@users.noreply.github.com>
2021-04-19 17:45:35 -0400

    Add files via upload

---
+++ master.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+
+### setup terminal
+apt-get install -y bash-completion binutils
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
+
+
+### install k8s and containerd
+apt update
+apt install -y apt-transport-https
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt update
+
+apt install -y kubelet kubeadm kubectl containerd
+apt-mark hold kubelet kubeadm kubectl containerd
+
+# Load the necessary modules for Containerd:
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Setup the required kernel parameters:
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
+
+
+### Configure containerd:
+
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+### Set up Master node
+
+sudo kubeadm init --cri-socket /run/containerd/containerd.sock
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+
+
+
