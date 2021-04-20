5d055617bfc7dda0d73d358ed84be634f50c25a1
tstark7952 <38063000+tstark7952@users.noreply.github.com>
2021-04-20 19:22:57 -0400

    Create master-w-docker.sh

---
+++ master-w-docker.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+### install k8s and docker
+apt-get remove -y docker.io kubelet kubeadm kubectl kubernetes-cni
+apt-get autoremove -y
+apt-get install -y etcd-client vim build-essential
+
+systemctl daemon-reload
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+KUBE_VERSION=1.20.2
+apt-get update
+apt-get install -y docker.io kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 kubernetes-cni=0.8.7-00
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "storage-driver": "overlay2"
+}
+EOF
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
+
+# start docker on reboot
+systemctl enable docker
+
+docker info | grep -i "storage"
+docker info | grep -i "cgroup"
+
+systemctl enable kubelet && systemctl start kubelet
+
+
+### init k8s
+rm /root/.kube/config
+kubeadm reset -f
+kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print
+
+mkdir -p ~/.kube
+sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+echo
+echo "### COMMAND TO ADD A WORKER NODE ###"
+kubeadm token create --print-join-command --ttl 0
