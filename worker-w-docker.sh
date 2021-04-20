c912e2a25073b4bbf94599afc174f14bf10872d5
tstark7952 <38063000+tstark7952@users.noreply.github.com>
2021-04-20 19:25:35 -0400

    Create worker-w-docker.sh

---
+++ worker-w-docker.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+
+### install k8s and docker
+apt-get remove -y docker.io kubelet kubeadm kubectl kubernetes-cni
+apt-get autoremove -y
+apt-get install -y etcd-client vim build-essential
+
+systemctl daemon-reload
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+KUBE_VERSION=1.20.2
+apt-get update
+apt-get install -y docker.io kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 kubernetes-cni=0.8.7-00
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "storage-driver": "overlay2"
+}
+EOF
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
+
+# start docker on reboot
+systemctl enable docker
+
+docker info | grep -i "storage"
+docker info | grep -i "cgroup"
+
+systemctl enable kubelet && systemctl start kubelet
+
+
+### init k8s
+kubeadm reset -f
+systemctl daemon-reload
+service kubelet start
+
+echo
+echo "EXECUTE ON MASTER: kubeadm token create --print-join-command --ttl 0"
+echo "THEN RUN THE OUTPUT AS COMMAND HERE TO ADD AS WORKER"
+echo
