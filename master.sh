7d5cced338545ef2656cb5617873bcbb729f2074
tstark7952 <38063000+tstark7952@users.noreply.github.com>
2021-05-08 12:02:18 -0400

    Update master.sh

--- master.sh
+++ master.sh
@@ -6,22 +6,13 @@ apt-get install -y bash-completion binutils
 echo 'source <(kubectl completion bash)' >> ~/.bashrc
 echo 'alias k=kubectl' >> ~/.bashrc
 
+### On all nodes, disable swap.
 
-### install k8s and containerd
-apt update
-apt install -y apt-transport-https
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-apt update
-
-apt install -y kubelet kubeadm kubectl containerd
-
-# Load the necessary modules for Containerd:
+### ContainerD setup and config
 
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
@@ -29,24 +20,42 @@ br_netfilter
 EOF
 
 sudo modprobe overlay
+
 sudo modprobe br_netfilter
 
-# Setup the required kernel parameters:
-
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward = 1
 EOF
 
 sudo sysctl --system
 
-
-### Configure containerd:
+sudo apt-get update && sudo apt-get install -y containerd
 
 sudo mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
 sudo systemctl restart containerd
+
+### install kubeadm, kubelet, and kubectl
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+sudo apt-get update
+
+sudo apt-get install -y kubelet kubeadm kubectl
+
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16
+
+
 
 ### Set up Master node
 
@@ -54,7 +63,4 @@ sudo kubeadm init --cri-socket /run/containerd/containerd.sock
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
-
-
-
+kubectl apply -f https://docs.projectcalico.org/v3.19/manifests/calico.yaml
