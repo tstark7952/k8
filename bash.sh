d20c35e94fcbb3b066ca9c279421325e4fedc108
tstark7952 <38063000+tstark7952@users.noreply.github.com>
2021-11-17 11:29:44 -0500

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,12 @@
+### setup terminal
+apt-get update
+apt-get install -y bash-completion binutils
+echo 'colorscheme ron' >> ~/.vimrc
+echo 'set tabstop=2' >> ~/.vimrc
+echo 'set shiftwidth=2' >> ~/.vimrc
+echo 'set expandtab' >> ~/.vimrc
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
+echo 'alias c=clear' >> ~/.bashrc
+echo 'complete -F __start_kubectl k' >> ~/.bashrc
+sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
