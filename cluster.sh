26d5ab01a023834ab873c76d347788934e3a0865
tstark7952 <38063000+tstark7952@users.noreply.github.com>
2022-11-04 14:47:56 -0400

    Create cluster.sh

---
+++ cluster.sh
@@ -0,0 +1,2 @@
+
+kubectl apply -f 'https://install.portworx.com/2.12?operator=true&mc=false&b=true&j=auto&c=px-cluster-8d391b92-828d-4a05-be77-e37e8296bd6f&stork=true&csi=true&mon=true&tel=false&st=k8s&promop=true'
