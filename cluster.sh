8aa2aa6922b0a2bf3e36ee18729353a9af9dad48
tstark7952 <38063000+tstark7952@users.noreply.github.com>
2022-11-04 14:48:32 -0400

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 
 kubectl apply -f 'https://install.portworx.com/2.12?operator=true&mc=false&b=true&j=auto&c=px-cluster-8d391b92-828d-4a05-be77-e37e8296bd6f&stork=true&csi=true&mon=true&tel=false&st=k8s&promop=true'
